cc132e22e169e1080357e20444a12ba65d875754
Harry <harrydawson90@hotmail.co.uk>
2022-02-28 15:28:04 +0000

    added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
+  it 'card holds money' do
+    expect(subject.balance).to be_a Integer
+  end
+
+  it 'card can be topped up' do
+    expect(subject.top_up(10)).to eq 10
+  end
 end
