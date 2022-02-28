4adcbacd4543ae24f89f6261e66e61749b27396f
Harry <harrydawson90@hotmail.co.uk>
2022-02-28 15:58:53 +0000

    added fare method and charging test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,18 @@
 class Oystercard
   attr_accessor :balance
   DEFAULT_LIMIT = 90
-  def initialize
-    @balance = 0
+  def initialize(balance = 0)
+    @balance = balance
   end
 
   def top_up(money)
-    
     raise "exceeds £#{DEFAULT_LIMIT} balance limit" if @balance + money > DEFAULT_LIMIT
     @balance += money
-
   end
 
+  def fare(cost)
+    @balance -= cost
+    
+    @balance
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,4 +12,8 @@ describe Oystercard do
   it 'raises an error when balance exceeds £90 limit' do
     expect{ subject.top_up(91)}.to raise_error "exceeds £90 balance limit"
   end
+
+  it 'deducts money from the balance' do
+    expect{ subject.fare(3) }.to change{ subject.balance }.by -3
+  end
 end
